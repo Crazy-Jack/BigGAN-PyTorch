aea7fee4b542257b61c1e6d5b533e7286e1715c1
Tianqin Li <jacklitianqin@gmail.com>
2021-04-24 22:31:41 -0400

    init concept attention

--- layer_concept_attention.py
+++ layer_concept_attention.py
@@ -1,20 +1,32 @@
-''' Layers
-    This file contains various layers for the BigGAN models.
-'''
-import functools
+import torch 
+import torch.nn as nn 
+import torchvision 
+import os
 
-import numpy as np
-import torch
-import torch.nn as nn
-from torch.nn import init
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.nn import Parameter as P
-from torch.autograd import Variable
 
-from sync_batchnorm import SynchronizedBatchNorm2d as SyncBN2d
 
-from layer_conv_select import SparseNeuralConv
-from layer_vc_linear_comb import LinearCombineVC
-from layer_conv_select_multiple_path import SparseNeuralConvMulti
-from torch.distributions import Categorical
+class ConceptPool(nn.Module):
+    """concept pool that contains multiple concepts"""
+    def __init__(self, num_k=20, pool_size_per_cluster=100, feature_dim=128):
+        
+
+
+
+
+
+
+class ConceptAttentionProto(nn.Module):
+    """concept attention with prototype to reduce attention time"""
+    def __init__(self, topk, visual_concept_pool_size, visual_concept_dim, mode, lambda_l1_reg_dot=1, test=False):
+        super(ConceptAttentionProto, self).__init__()
+        self.register_buffer('', torch.zeros(K, dtype=torch.long))
+        
+    def forward(self, x, device="cuda"):
+        n, c, h, w = x.shape 
+
+        # 
+
+
+
+
+
