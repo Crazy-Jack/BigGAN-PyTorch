40fef921bbcac7c5c1d925c7f2b37e2044bb4288
Tianqin Li <jacklitianqin@gmail.com>
2021-04-25 12:05:31 -0400

    add faiss kmeans

--- layer_concept_attention.py
+++ layer_concept_attention.py
@@ -59,10 +59,75 @@ class ConceptAttentionProto(nn.Module):
         
         self.concept_pool[:, index] = content 
     
-    
-    def pool_kmean_init(self):
-        """perform kmeans for cluster concept pool initialization"""
+    def pool_kmean_init(self, seed=0, gpu_num=0, temperature=1):
+        """TODO: clear up
+        perform kmeans for cluster concept pool initialization
+        Args:
+            x: data to be clustered
+        """
         
+        print('performing kmeans clustering')
+        results = {'im2cluster':[],'centroids':[],'density':[]}
+        x = self.concept_pool.clone().cpu().numpy()
+        num_cluster = self.num_k
+        # intialize faiss clustering parameters
+        d = x.shape[1]
+        k = int(num_cluster)
+        clus = faiss.Clustering(d, k)
+        clus.verbose = True
+        clus.niter = 20
+        clus.nredo = 5
+        clus.seed = seed
+        clus.max_points_per_centroid = 1000
+        clus.min_points_per_centroid = 10
+
+        res = faiss.StandardGpuResources()
+        cfg = faiss.GpuIndexFlatConfig()
+        cfg.useFloat16 = False
+        cfg.device = gpu_num   
+        index = faiss.GpuIndexFlatL2(res, d, cfg)  
+
+        clus.train(x, index)   
+
+        D, I = index.search(x, 1) # for each sample, find cluster distance and assignments
+        im2cluster = [int(n[0]) for n in I]
+        
+        # get cluster centroids
+        centroids = faiss.vector_to_array(clus.centroids).reshape(k,d)
+        
+        # sample-to-centroid distances for each cluster 
+        Dcluster = [[] for c in range(k)]          
+        for im,i in enumerate(im2cluster):
+            Dcluster[i].append(D[im][0])
+        
+        # concentration estimation (phi)        
+        density = np.zeros(k)
+        for i,dist in enumerate(Dcluster):
+            if len(dist)>1:
+                d = (np.asarray(dist)**0.5).mean()/np.log(len(dist)+10)            
+                density[i] = d     
+                
+        #if cluster only has one point, use the max to estimate its concentration        
+        dmax = density.max()
+        for i,dist in enumerate(Dcluster):
+            if len(dist)<=1:
+                density[i] = dmax 
+
+        density = density.clip(np.percentile(density,10),np.percentile(density,90)) #clamp extreme values for stability
+        density = temperature*density/density.mean()  #scale the mean to temperature 
+        
+        # convert to cuda Tensors for broadcast
+        centroids = torch.Tensor(centroids).cuda()
+        centroids = nn.functional.normalize(centroids, p=2, dim=1)    
+
+        im2cluster = torch.LongTensor(im2cluster).cuda()               
+        density = torch.Tensor(density).cuda()
+        
+        results['centroids'].append(centroids)
+        results['density'].append(density)
+        results['im2cluster'].append(im2cluster)    
+        
+        return results    
 
 
 
